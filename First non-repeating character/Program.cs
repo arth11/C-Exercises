c3344dddd3a5eeb498698b359a634ff8f1dfffa3
Artur Chojnacki <arc@nclan.netcompany.dk>
2019-01-25 09:27:08 +0100

    Add First no repeatin character exercise

---
+++ First non-repeating character/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace First_non_repeating_character
+{
+    class Program
+    {
+        public static string FirstNonRepeatingLetter(string s)
+        {
+          
+            if (s.Length == 1) return s.Substring(0, 1);
+           
+            Dictionary<string, int> myDict = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            foreach(var temp in s)
+            {
+                if (myDict.ContainsKey(temp.ToString())) myDict[temp.ToString()] +=1;
+                else myDict.Add(temp.ToString(), 0);
+            }
+
+            foreach (var item in myDict) 
+            {
+                if (item.Value == 0) return item.Key.ToString();
+            }
+            
+
+            return String.Empty;
+        }
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstNonRepeatingLetter("stress"));
+            Console.Read();
+        }
+    }
+
+}
+
+
