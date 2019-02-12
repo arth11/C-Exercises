af654fcd3485ac5418410ba397c4c096657e4b4a
Artur Chojnacki <arc@nclan.netcompany.dk>
2019-02-12 09:46:37 +0100

    Google Tag Manager from backend

---
+++ GoogleAnalitics/GoogleAnalitics/GoogleAnaliticsItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleAnalitics
+{
+   public class GoogleAnaliticsItem
+    {
+        public string AdSenseId_a { get; set; } //   388883736 
+        //public string ApiVersion_v { get; set; } //   1
+        //public string ClientId_cid { get; set; } //  1831828158.1532331218
+        //public string Id_ti { get; set; } //  1011576415
+       public string ItemCode_ic { get; set; } //  5430001111
+        //public string ItemName_in { get; set; }  // RØRHOLDER LYSGRÅ M32 FOR RØR
+        //public string ItemPrice_ip { get; set; } //  2.5899999141693115
+        //public string ItemQuantity_iq { get; set; } //  50
+        //public string ItemVariation_iv { get; set; } // RØRHOLDER LYSGRÅ M32 FOR RØR
+        //public string Encoding_de { get; set; } //UTF-8
+        //public string HitType_t { get; set; }  // item
+        //public string Language_ul { get; set; } // en-us
+        //public string Location_dl { get; set; } // http://qa.webshop.solar.dk/send/12932730
+        //public string Title_dt { get; set; } //  Solar WebShop v.1.5.277.0
+        //public string TrackingId_tid { get; set; } //  UA-7225725-62
+        //public string Revenue_tr { get; set; } // 5490.140132427216
+        //public string EventAction_ea { get; set; } //   Send Order
+        //public string EventCategory_ec { get; set; }  //   Conversion
+        //public string EventLabel_el { get; set; } //  /send/12932730
+
+    }
+}
---
+++ GoogleAnalitics/GoogleAnalitics/GoogleAnalyticsApi.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace GoogleAnalitics
+{
+  public  class GoogleAnalyticsApi
+    {
+
+        
+    static void Main(string[] args)
+        {
+            GoogleAnaliticsItem item = new GoogleAnaliticsItem()
+            {
+                AdSenseId_a = "1",
+                ItemCode_ic = "5430001111"
+            };
+
+            Track(item);
+        }
+
+        private static void Track(GoogleAnaliticsItem item)
+        {
+            if (string.IsNullOrEmpty(item.AdSenseId_a)) item.AdSenseId_a = "empty"; 
+            if (string.IsNullOrEmpty(item.ItemCode_ic)) item.ItemCode_ic = "empty";
+
+            var request = (HttpWebRequest)WebRequest.Create("https://www.google-analytics.com/collect");
+            request.Method = "POST";
+
+            // the request body we want to send
+            var postData = new Dictionary<string, string>
+                           {
+                               { "v", "1" },
+                               { "tid", "UA-7225725-62" }, //QA
+                               { "cid", "555" },
+                               { "t", item.AdSenseId_a },
+                               { "ec", item.ItemCode_ic }
+
+                           };
+            //if (!string.IsNullOrEmpty(label))
+            //{
+            //    postData.Add("el", label);
+            //}
+            //if (value.HasValue)
+            //{
+            //    postData.Add("ev", value.ToString());
+            //}
+
+            var postDataString = postData
+                .Aggregate("", (data, next) => string.Format("{0}&{1}={2}", data, next.Key,
+                                                             next.Value))
+                .TrimEnd('&');
+
+            //  set the Content - Length header to the correct value
+            request.ContentLength = Encoding.UTF8.GetByteCount(postDataString);
+
+            // write the request body to the request
+            using (var writer = new StreamWriter(request.GetRequestStream()))
+            {
+
+                writer.Write(postDataString);
+            }
+
+            try
+            {
+                var webResponse = (HttpWebResponse)request.GetResponse();
+                if (webResponse.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new Exception("Google Analytics tracking did not return OK 200");
+                    //new Exception((int)webResponse.StatusCode,
+                    //                      "Google Analytics tracking did not return OK 200");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                // do what you like here, we log to Elmah
+                // ElmahLog.LogError(ex, "Google Analytics tracking failed");
+            }
+        }
+
+        //public static void TrackEvent(GoogleAnaliticsItem analiticsItem)
+        //{
+        //    Track(HitType.@event, category, action, label, value);
+        //}
+
+        //public static void TrackPageview(string category, string action, string label, int? value = null)
+        //{
+        //    Track(HitType.@pageview, category, action, label, value);
+        //}
+
+     
+
+       
+    }
+}
